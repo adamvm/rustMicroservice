bed0c980e03a899c7e07de96aa6e1d8d4aa4667c
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-09-25 18:04:10 +0200

    Simple microservice with hyper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use hyper::{Body, Response, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::Future;
+
 fn main() {
-    println!("Hello, world!");
+
+    let addr = ([127, 0 ,0, 1], 3030).into();
+    let builder = Server::bind(&addr);
+
+    let server = builder.serve(|| {
+        service_fn_ok(|_| {
+            Response::new(Body::from("Almost microservice..."))
+        })
+    });
+    let server = server.map_err(drop);
+    hyper::rt::run(server)
 }
