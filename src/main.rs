581a2ed78243f579555fcc738ec31c7522dcbcc6
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-09-26 06:59:31 +0200

    Microservice handler introduction

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
-use hyper::{Body, Response, Server};
-use hyper::service::service_fn_ok;
-use hyper::rt::Future;
+use futures::{future, Future};
+use hyper::{Body, Error, Method, Response, Server, Request, StatusCode};
+use hyper::service::service_fn;
+
+const INDEX: &'static str = r#"
+<!doctype html>
+<html>
+     <head>
+          <title>Rust Microservice</title>
+     </head>
+     <body>
+          <h3>Rust Microservice</h3>
+     </body>
+</html>
+"#;
+
+
+fn microservice_handler(req: Request<Body>)
+    -> impl Future<Item=Response<Body>, Error=Error>
+{
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            future::ok(Response::new(INDEX.into()))
+        },
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+            future::ok(response)
+        },
+    }
+}
 
 fn main() {
-
-    let addr = ([127, 0 ,0, 1], 3030).into();
+    let addr = ([127, 0, 0, 1], 3030).into();
     let builder = Server::bind(&addr);
 
-    let server = builder.serve(|| {
-        service_fn_ok(|_| {
-            Response::new(Body::from("Almost microservice..."))
-        })
-    });
+    let server = builder.serve(|| service_fn(microservice_handler));
     let server = server.map_err(drop);
     hyper::rt::run(server)
 }
+
+
