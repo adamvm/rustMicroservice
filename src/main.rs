2c548acc092446028ce3a4fde2c46dcc2273ef7f
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-09-26 07:35:48 +0200

    Add user id handling with Slab

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::fmt;
 use futures::{future, Future};
-use hyper::{Body, Error, Method, Response, Server, Request, StatusCode};
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
+use std::sync::{Arc, Mutex};
+use slab::Slab;
 
 const INDEX: &'static str = r#"
 <!doctype html>
@@ -14,29 +17,37 @@ const INDEX: &'static str = r#"
 </html>
 "#;
 
+type UserId = u64;
 
-fn microservice_handler(req: Request<Body>)
-    -> impl Future<Item=Response<Body>, Error=Error>
+struct UserData;
+
+type UserDb = Arc<Mutex<Slab<UserData>>>;
+
+fn microservice_handler(req: Request<Body>, user_db: &UserDb)
+                        -> impl Future<Item=Response<Body>, Error=Error>
 {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
             future::ok(Response::new(INDEX.into()))
-        },
+        }
         _ => {
             let response = Response::builder()
                 .status(StatusCode::NOT_FOUND)
                 .body(Body::empty())
                 .unwrap();
             future::ok(response)
-        },
+        }
     }
 }
 
 fn main() {
     let addr = ([127, 0, 0, 1], 3030).into();
     let builder = Server::bind(&addr);
-
-    let server = builder.serve(|| service_fn(microservice_handler));
+    let user_db = Arc::new(Mutex::new(Slab::new()));
+    let server = builder.serve(move || {
+        let user_db = user_db.clone();
+        service_fn(move |req| microservice_handler(req, &user_db))
+    });
     let server = server.map_err(drop);
     hyper::rt::run(server)
 }
