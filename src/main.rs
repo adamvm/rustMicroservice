9d1a60304a9f67cd3224b3810341eadbe3e86a46
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-09-26 13:30:20 +0200

    Introduce regex for request matching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::fmt;
 use std::sync::{Arc, Mutex};
 use slab::Slab;
+use lazy_static::lazy_static;
 use futures::{future, Future};
 use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
+use regex::Regex;
 
 const INDEX: &str = r#"
 <!doctype html>
@@ -29,61 +31,79 @@ impl fmt::Display for UserData {
 
 type UserDb = Arc<Mutex<Slab<UserData>>>;
 
-const USER_PATH: &str = "/user/";
+lazy_static! {
+    static ref INDEX_PATH: Regex = Regex::new("^/(index\\.html?)?$").unwrap();
+    static ref USER_PATH: Regex = Regex::new("^/user/((?P<user_id>\\d+?)/?)?$").unwrap();
+    static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
+}
 
 fn microservice_handler(req: Request<Body>, user_db: &UserDb)
                         -> impl Future<Item=Response<Body>, Error=Error>
 {
     let response = {
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/") => {
+        let method = req.method();
+        let path = req.uri().path();
+        let mut users = user_db.lock().unwrap();
+        if INDEX_PATH.is_match(path) {
+            if method == &Method::GET {
                 Response::new(INDEX.into())
-            },
-            (method, path) if path.starts_with(USER_PATH) => {
-                let user_id = path.trim_start_matches(USER_PATH)
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if USERS_PATH.is_match(path) {
+            if method == &Method::GET {
+                let list = users.iter()
+                    .map(|(id, _)| id.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",");
+                Response::new(list.into())
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if let Some(cap) = USER_PATH.captures(path) {
+            let user_id = cap.name("user_id").and_then(|m| {
+                m.as_str()
                     .parse::<UserId>()
                     .ok()
-                    .map(|x| x as usize);
-                let mut users = user_db.lock().unwrap();
-                match (method, user_id) {
-                    (&Method::GET, Some(id)) => {
-                        if let Some(data) = users.get(id) {
-                            Response::new(data.to_string().into())
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    },
-                    (&Method::POST, None) => {
-                        let id = users.insert(UserData);
-                        Response::new(id.to_string().into())
-                    },
-                    (&Method::POST, Some(_)) => {
-                        response_with_code(StatusCode::BAD_REQUEST)
-                    },
-                    (&Method::PUT, Some(id)) => {
-                        if let Some(user) = users.get_mut(id) {
-                            *user = UserData;
-                            response_with_code(StatusCode::OK)
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    },
-                    (&Method::DELETE, Some(id)) => {
-                        if users.contains(id) {
-                            users.remove(id);
-                            response_with_code(StatusCode::OK)
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    },
-                    _ => {
-                        response_with_code(StatusCode::METHOD_NOT_ALLOWED)
-                    },
-                }
-            },
-            _ => {
-                response_with_code(StatusCode::NOT_FOUND)
-            },
+                    .map(|x| x as usize)
+            });
+            match (method, user_id) {
+                (&Method::GET, Some(id)) => {
+                    if let Some(data) = users.get(id) {
+                        Response::new(data.to_string().into())
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                },
+                (&Method::POST, None) => {
+                    let id = users.insert(UserData);
+                    Response::new(id.to_string().into())
+                },
+                (&Method::POST, Some(_)) => {
+                    response_with_code(StatusCode::BAD_REQUEST)
+                },
+                (&Method::PUT, Some(id)) => {
+                    if let Some(user) = users.get_mut(id) {
+                        *user = UserData;
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                },
+                (&Method::DELETE, Some(id)) => {
+                    if users.contains(id) {
+                        users.remove(id);
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                },
+                _ => {
+                    response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+                },
+            }
+        } else {
+            response_with_code(StatusCode::NOT_FOUND)
         }
     };
     future::ok(response)
